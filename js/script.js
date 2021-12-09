91192a9e2ad47d8c9a26598a01f5054d170fe2ea
Lucas Lopes <Lucas.santos.pessoal@outlook.com>
2021-12-09 08:12:20 -0300

    Iniciando o arquivo JavaScript

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//Carne- 400mg por pessoa, mais de 6h - 650mg
+//Cerveja - 1.2 litro por pessoa, mais de 6h - 2 litros
+//Refrigerente/água 1 litro por pessoa, mais de 6h - 1.5 litros
+//crianças valem por 0.5 'pessoa
+
+let adulto = document.querySelector('#adulto');
