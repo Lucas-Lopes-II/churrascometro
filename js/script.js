adabd56b17090a51795fbda14d67d426b334282e
Lucas Lopes <Lucas.santos.pessoal@outlook.com>
2021-12-09 14:08:46 -0300

    Iniciando o btn.onclick

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@
 //crianças valem por 0.5 'pessoa
 
 let adulto = document.querySelector('#adulto');
+let crianca = document.querySelector('#crianca');
+let duracao = document.querySelector('#duracao');
+let btn = document.querySelector('#btn');
+
+btn.onclick = function() {
+    let pessoas = (parseFloat(aultos.value) + parseFloat((crianca.value / 2))).toFixed(2);
+    let horas = (parseFloat(duracao.value)).toFixed(2);  
+    
+}
